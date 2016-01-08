c9376d55aff66dbc8ac863f1a846eeae6016080b
Minsheng Liu <lambda@liu.ms>
2016-01-08 04:34:48 -0800

    base hskribo on blaze

--- src/Text/Hskribo.hs
+++ src/Text/Hskribo.hs
@@ -1,68 +1,34 @@
-{-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE GADTs                #-}
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE KindSignatures       #-}
 {-# LANGUAGE OverloadedStrings    #-}
-{-# LANGUAGE TypeSynonymInstances #-}
 
-module Text.Hskribo where
+module Text.Hskribo
+  ( module Text.Hskribo.Type
+  , text, emph, def, heading
+  , par, section, aside
+  ) where
 
-import           Data.Monoid ((<>))
-import qualified Data.Text   as T
+import Data.Text (Text)
+import Text.Blaze.Internal (customParent)
+import Text.Hskribo.Type
 
-import Servant.ContentType.PlainHtml
+import qualified Text.Blaze.Html5 as H
 
-data HskriboDocIx = IxSection | IxInline
-
-type IxSection = 'IxSection
-type IxInline  = 'IxInline
-
-data HskriboDoc (ix :: HskriboDocIx) where
-  SectionNode   :: TagType -> [HskriboDoc IxSection] -> HskriboDoc IxSection
-  ParagraphNode :: TagType -> [HskriboDoc IxInline]  -> HskriboDoc IxSection
-  InlineNode    :: TagType -> [HskriboDoc IxInline]  -> HskriboDoc IxInline
-  TextNode      :: T.Text -> HskriboDoc IxInline
-
-newtype TagType = TagType { unTagType :: T.Text }
-
-type SectionNode = HskriboDoc IxSection
-type InlineNode  = HskriboDoc IxInline
-type Document    = HskriboDoc IxSection
-
-text :: T.Text -> InlineNode
+text :: Text -> InlineNode
 text = TextNode
 
 emph :: [InlineNode] -> InlineNode
-emph = InlineNode $ TagType "em"
+emph = InlineNode H.em
 
 def :: [InlineNode] -> InlineNode
-def = InlineNode $ TagType "def"
+def = InlineNode $ customParent "def"
 
 heading :: [InlineNode] -> SectionNode
-heading = ParagraphNode $ TagType "h1"
+heading = ParagraphNode H.h1
 
 par :: [InlineNode] -> SectionNode
-par = ParagraphNode $ TagType "p"
+par = ParagraphNode H.p
 
 section :: [SectionNode] -> SectionNode
-section = SectionNode $ TagType "section"
+section = SectionNode H.section
 
 aside :: [SectionNode] -> SectionNode
-aside = SectionNode $ TagType "aside"
-
-instance ToPlainHtml SectionNode where
-  toPlainHtml (SectionNode tag children) = T.concat $
-    [start] <> map toPlainHtml children <> [end]
-    where start = "<"  <> unTagType tag <> ">\n"
-          end   = "</" <> unTagType tag <> ">\n"
-  toPlainHtml (ParagraphNode tag children) = T.concat $
-    [start] <> map toPlainHtml children <> [end]
-    where start = "<"  <> unTagType tag <> ">"
-          end   = "</" <> unTagType tag <> ">\n"
-
-instance ToPlainHtml InlineNode where
-  toPlainHtml (InlineNode tag children) = T.concat $
-    [start] <> map toPlainHtml children <> [end]
-    where start = "<"  <> unTagType tag <> ">"
-          end   = "</" <> unTagType tag <> ">"
-  toPlainHtml (TextNode txt) = txt
+aside = SectionNode H.aside
